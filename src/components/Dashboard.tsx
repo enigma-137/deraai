2dd835e50c79dfceb446241178959b8d47177ef6
enigma-devhub <emmanuelonoja291@gmail.com>
2023-11-18 13:25:59 +0100

    hgg

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,7 +8,7 @@ import Skeleton from 'react-loading-skeleton'
 import Link from 'next/link'
 import format from 'date-fns/format'
 import { Button } from './ui/button'
-import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getUserSubscriptionPlan } from '@/libs/stripe'
 
 interface PageProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
